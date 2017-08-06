0d6833129010104e3fa84fab9d1ab51952c65327
sambrosia <samuelcwoodruff@gmail.com>
2017-08-05 18:58:31 -0600

    Add existing module
    Tracks which keys are pressed by name and key code.
    Key names are transformed to be less stupid than the defaults.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
+const pressedKeys = {}
+const pressedCodes = {}
 
+function isKeyDown (key) {
+  return pressedKeys[key] || false
+}
 
+function isCodeDown (code) {
+  return pressedCodes[code] || false
+}
+
+function transformKeyName (key) {
+  return key.toLowerCase()
+    .replace(/^arrow/, '')
+    .replace(/(^\s$)|spacebar/, 'space')
+}
+
+document.addEventListener('keydown', event => {
+  const key = transformKeyName(event.key)
+  const code = event.code
+
+  pressedKeys[key] = true
+  pressedCodes[code] = true
+})
+
+document.addEventListener('keyup', event => {
+  const key = transformKeyName(event.key)
+  const code = event.code
+
+  pressedKeys[key] = false
+  pressedCodes[code] = false
+})
+
+export default { isKeyDown, isCodeDown }
