68742b76fe4e45e925c7f0f2c46b39242527b180
sambrosia <samuelcwoodruff@gmail.com>
2017-08-06 10:56:26 -0600

    Add buble and uglify to build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,18 @@
+import filesize from 'rollup-plugin-filesize'
+import buble from 'rollup-plugin-buble'
+import uglify from 'rollup-plugin-uglify'
+
 export default {
   entry: 'src/index.js',
   dest: 'dist/kaybee.js',
   sourceMap: true,
 
   format: 'umd',
-  moduleName: 'kaybee'
+  moduleName: 'kaybee',
+
+  plugins: [
+    filesize(),
+    buble(),
+    uglify()
+  ]
 }
