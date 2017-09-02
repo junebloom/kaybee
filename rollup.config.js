bfb803093047555505a738a3bb77ad32a8e35d94
sambrosia <samuelcwoodruff@gmail.com>
2017-09-02 12:20:37 -0600

    Fix missing rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
-import filesize from 'rollup-plugin-filesize'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import buble from 'rollup-plugin-buble'
 import uglify from 'rollup-plugin-uglify'
+import filesize from 'rollup-plugin-filesize'
 
 export default {
   entry: 'src/index.js',
@@ -11,8 +13,10 @@ export default {
   moduleName: 'kaybee',
 
   plugins: [
-    filesize(),
+    resolve(),
+    commonjs(),
     buble(),
-    uglify()
+    uglify(),
+    filesize()
   ]
 }
