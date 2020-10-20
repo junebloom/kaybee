5d61bf99080ebe149032eb349fff9de9ecc55c4c
June Bloom <june.a.bloom@gmail.com>
2020-10-19 17:15:33 -0700

    Update configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,12 @@
-import filesize from 'rollup-plugin-filesize'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import json from 'rollup-plugin-json'
-import babel from 'rollup-plugin-babel'
-import { uglify } from 'rollup-plugin-uglify'
-import { version } from './package.json'
-
-const preamble = `// kaybee ${version} | MIT License | Copyright (c) 2018 Sam Woodruff`
+import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: 'src/Kaybee.js',
+  input: "src/Kaybee.js",
   output: {
-    file: 'dist/Kaybee.js',
-    sourceMap: true,
-    format: 'umd',
-    name: 'kaybee'
+    file: "dist/Kaybee.js",
+    sourcemap: true,
+    format: "es",
+    name: "kaybee",
   },
-  plugins: [
-    filesize(),
-    resolve(),
-    commonjs(),
-    json(),
-    babel(),
-    uglify({
-      output: { preamble }
-    })
-  ]
-}
+  plugins: [terser()],
+};
