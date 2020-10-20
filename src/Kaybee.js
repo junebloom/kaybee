5b5594fc9d5c0262cb18381c2eb1296794841da9
June Bloom <june.a.bloom@gmail.com>
2020-10-19 17:21:54 -0700

    Format with prettier

--- src/Kaybee.js
+++ src/Kaybee.js
@@ -1,50 +1,50 @@
-import EventEmitter from 'eventemitter3'
+import EventEmitter from "eventemitter3";
 
 class Kaybee extends EventEmitter {
   constructor(options = {}) {
-    super()
+    super();
 
     this.options = Object.assign(
       { renameKeys: true, repeat: true, listen: true },
       options
-    )
-    this.pressedKeys = {}
-    this.pressedCodes = {}
+    );
+    this.pressedKeys = {};
+    this.pressedCodes = {};
 
     if (document && this.options.listen) {
-      document.addEventListener('keydown', this.handleKeyEvent.bind(this))
-      document.addEventListener('keyup', this.handleKeyEvent.bind(this))
+      document.addEventListener("keydown", this.handleKeyEvent.bind(this));
+      document.addEventListener("keyup", this.handleKeyEvent.bind(this));
     }
   }
 
   isKeyDown(key) {
-    return this.pressedKeys[key] || false
+    return this.pressedKeys[key] || false;
   }
 
   isCodeDown(code) {
-    return this.pressedCodes[code] || false
+    return this.pressedCodes[code] || false;
   }
 
   handleKeyEvent(event) {
-    if (!this.options.repeat && event.repeat) return
-    const pressed = event.type === 'keydown'
+    if (!this.options.repeat && event.repeat) return;
+    const pressed = event.type === "keydown";
     const key = this.options.renameKeys
       ? Kaybee.transformKeyName(event.key)
-      : event.key
-    const code = event.code
+      : event.key;
+    const code = event.code;
 
-    this.pressedKeys[key] = pressed
-    this.pressedCodes[code] = pressed
+    this.pressedKeys[key] = pressed;
+    this.pressedCodes[code] = pressed;
 
-    this.emit(event.type, key, code, event.repeat)
+    this.emit(event.type, key, code, event.repeat);
   }
 
   static transformKeyName(key) {
     return key
       .toLowerCase()
-      .replace(/^arrow/, '')
-      .replace(/(^\s$)|spacebar/, 'space')
+      .replace(/^arrow/, "")
+      .replace(/(^\s$)|spacebar/, "space");
   }
 }
 
-export default Kaybee
+export default Kaybee;
